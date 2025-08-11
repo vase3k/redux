3bc4a801104768d7b6bbdb1effbdc042f1f9c548
Vasiliy Vinogradov <vase3k@gmail.com>
2025-08-11 23:35:04 +0300

    redux createStore

--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { legacy_createStore as createStore } from 'redux';
+
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
-);
+const initialState = { value: 0 };
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC':
+            return { ...state, value: state.value + 1 };
+        case 'DEC':
+            return { ...state, value: state.value - 1 };
+        case 'RND':
+            state = { ...state, value: 1 };
+            return { ...state, value: state.value * action.payload };
+        default:
+            return { ...state };
+    }
+};
+
+const store = createStore(reducer);
+
+const update = () => {
+    document.querySelector('#counter').textContent = store.getState().value;
+};
+
+store.subscribe(update);
+
+const inc = () => ({ type: 'INC' });
+const dec = () => ({ type: 'DEC' });
+const rnd = value => ({ type: 'RND', payload: value });
+
+document.querySelector('#inc').addEventListener('click', () => {
+    store.dispatch(inc());
+});
+document.querySelector('#dec').addEventListener('click', () => {
+    store.dispatch(dec());
+});
+document.querySelector('#rnd').addEventListener('click', () => {
+    const value = (Math.random() * 100).toFixed(0);
+    store.dispatch(rnd(value));
+});
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <StrictMode>
+//         <></>
+//     </StrictMode>
+// );
