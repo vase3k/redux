b2f139ceadb1a9e2211d9a37983183b535570e13
Vasiliy Vinogradov <vase3k@gmail.com>
2025-08-12 11:32:50 +0300

    bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,46 +1,30 @@
+// eslint-disable-next-line
 import { StrictMode } from 'react';
+// eslint-disable-next-line
 import ReactDOM from 'react-dom/client';
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore, bindActionCreators } from 'redux';
+import * as actions from './actions';
+import reducer from './reducer';
 
 import './index.css';
 
-const initialState = { value: 0 };
-
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'INC':
-            return { ...state, value: state.value + 1 };
-        case 'DEC':
-            return { ...state, value: state.value - 1 };
-        case 'RND':
-            state = { ...state, value: 1 };
-            return { ...state, value: state.value * action.payload };
-        default:
-            return { ...state };
-    }
-};
-
 const store = createStore(reducer);
 
+const { dispatch, getState, subscribe } = store;
+
 const update = () => {
-    document.querySelector('#counter').textContent = store.getState().value;
+    document.querySelector('#counter').textContent = getState().value;
 };
 
-store.subscribe(update);
+subscribe(update);
 
-const inc = () => ({ type: 'INC' });
-const dec = () => ({ type: 'DEC' });
-const rnd = value => ({ type: 'RND', payload: value });
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
-document.querySelector('#inc').addEventListener('click', () => {
-    store.dispatch(inc());
-});
-document.querySelector('#dec').addEventListener('click', () => {
-    store.dispatch(dec());
-});
+document.querySelector('#inc').addEventListener('click', inc);
+document.querySelector('#dec').addEventListener('click', dec);
 document.querySelector('#rnd').addEventListener('click', () => {
     const value = (Math.random() * 100).toFixed(0);
-    store.dispatch(rnd(value));
+    rnd(value);
 });
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
---
+++ src/reducer.js
@@ -0,0 +1,17 @@
+const initialState = { value: 0 };
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC':
+            return { ...state, value: state.value + 1 };
+        case 'DEC':
+            return { ...state, value: state.value - 1 };
+        case 'RND':
+            state = { ...state, value: 1 };
+            return { ...state, value: state.value * action.payload };
+        default:
+            return { ...state };
+    }
+};
+
+export default reducer;
