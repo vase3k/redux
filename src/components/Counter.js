223aabdd5de5caeadbacff7c7b4f2b06529474bc
Vasiliy Vinogradov <vase3k@gmail.com>
2025-08-12 18:22:32 +0300

    connect

---
+++ src/components/Counter.js
@@ -0,0 +1,44 @@
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+import { Component } from 'react';
+
+const Counter = ({ counter, inc, dec, rnd }) => {
+    return (
+        <div className="jumbotron">
+            <h1>{counter}</h1>
+            <button onClick={inc} className="btn btn-primary">
+                INC
+            </button>
+            <button onClick={dec} className="btn btn-primary">
+                DEC
+            </button>
+            <button onClick={rnd} className="btn btn-primary">
+                RND
+            </button>
+        </div>
+    );
+};
+
+// class Counter extends Component {
+//     render() {
+//         const { counter, inc, dec, rnd } = this.props;
+//         return (
+//             <div className="jumbotron">
+//                 <h1>{counter}</h1>
+//                 <button onClick={inc} className="btn btn-primary">
+//                     INC
+//                 </button>
+//                 <button onClick={dec} className="btn btn-primary">
+//                     DEC
+//                 </button>
+//                 <button onClick={rnd} className="btn btn-primary">
+//                     RND
+//                 </button>
+//             </div>
+//         );
+//     }
+// }
+
+const mapStateToProps = state => ({ counter: state.value });
+
+export default connect(mapStateToProps, actions)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,34 +1,17 @@
-// eslint-disable-next-line
 import { StrictMode } from 'react';
-// eslint-disable-next-line
 import ReactDOM from 'react-dom/client';
-import { legacy_createStore as createStore, bindActionCreators } from 'redux';
-import * as actions from './actions';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
 import reducer from './reducer';
-
+import App from './components/App';
 import './index.css';
 
 const store = createStore(reducer);
 
-const { dispatch, getState, subscribe } = store;
-
-const update = () => {
-    document.querySelector('#counter').textContent = getState().value;
-};
-
-subscribe(update);
-
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-
-document.querySelector('#inc').addEventListener('click', inc);
-document.querySelector('#dec').addEventListener('click', dec);
-document.querySelector('#rnd').addEventListener('click', () => {
-    const value = (Math.random() * 100).toFixed(0);
-    rnd(value);
-});
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//     <StrictMode>
-//         <></>
-//     </StrictMode>
-// );
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>
+);
